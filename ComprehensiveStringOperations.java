b632bcea159dee7719bffacc3c863e6f36d8a2d0
rakshithamg-max <rakshithamg70@gmail.com>
2026-04-20 06:19:42 +1400

    Add files via upload

---
+++ ComprehensiveStringOperations.java
@@ -0,0 +1,56 @@
+package prog3;
+import java.util.*;
+
+public class ComprehensiveStringOperations {
+    public static void main(String[] args) {
+
+        
+        String str1 = "Hello";
+        String str2 = "World";
+
+        
+        System.out.println("Length: " + str1.length());
+        System.out.println("Char at index 1: " + str1.charAt(1));
+
+        
+        System.out.println("Equals: " + str1.equals("Hello"));
+        System.out.println("CompareTo: " + str1.compareTo(str2));
+
+        
+        System.out.println("Contains 'lo': " + str1.contains("lo"));
+        System.out.println("Index of 'l': " + str1.indexOf('l'));
+
+        
+        System.out.println("Substring: " + str1.substring(1, 4));
+
+        
+        System.out.println("Uppercase: " + str1.toUpperCase());
+        System.out.println("Replace: " + str1.replace('l', 'x'));
+
+        
+        String str3 = "  Java Programming  ";
+        System.out.println("Trimmed: '" + str3.trim() + "'");
+
+
+        System.out.println("Concat: " + str1.concat(" " + str2));
+
+        String sentence = "Java is easy";
+        String[] words = sentence.split(" ");
+        for (String w : words) {
+            System.out.println(w);
+        }
+
+        StringBuilder sb = new StringBuilder("Hello");
+        sb.append(" Java");
+        System.out.println("StringBuilder: " + sb);
+
+        String formatted = String.format("Name: %s, Age: %d", "John", 20);
+        System.out.println(formatted);
+        String email = "test@gmail.com";
+        if (email.contains("@") && email.startsWith("test") && email.endsWith(".com")) {
+            System.out.println("Valid Email");
+        } else {
+            System.out.println("Invalid Email");
+        }
+    }
+}
