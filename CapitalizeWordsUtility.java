c8ca95980bbf8ec951cebb3cc73cdec1e7300ba9
rakshithamg-max <rakshithamg70@gmail.com>
2026-04-21 07:23:50 +1400

    Update CapitalizeWordsUtility.java

--- CapitalizeWordsUtility.java
+++ CapitalizeWordsUtility.java
@@ -1,3 +1,6 @@
+/*Write a Java Program for Capitalizing the first letter of each word. using user defined function capitalizeWords()*/
+
+
 package prog3c;
 public class CapitalizeWordsUtility {
     public static String capitalizeWords(String str) {
