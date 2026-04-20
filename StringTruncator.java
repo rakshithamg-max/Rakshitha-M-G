6ce32095e1f19347b8cb4d10657e539348ade377
rakshithamg-max <rakshithamg70@gmail.com>
2026-04-21 07:36:14 +1400

    Update StringTruncator.java

--- StringTruncator.java
+++ StringTruncator.java
@@ -1,3 +1,6 @@
+/* Write a Java Program for Shortening a string to a specified length and adds an ellipsis using user defined function truncate()
+
+*/
 package prog3c;
 public class StringTruncator {
     public static String truncate(String str, int length) {
