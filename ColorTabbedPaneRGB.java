af34f78f159e6629b1664e22749762b65c087a02
rakshithamg-max <rakshithamg70@gmail.com>
2026-04-21 07:26:44 +1400

    Update ColorTabbedPaneRGB.java

--- ColorTabbedPaneRGB.java
+++ ColorTabbedPaneRGB.java
@@ -1,3 +1,6 @@
+/* Develop a Swing program in Java to create a Tabbed Pan of RED, BLUE and GREEN and display the concerned color whenever the specific
+ tab is selected in the Pan.*/
+
 package prog5;
 import javax.swing.*;
 import java.awt.*;
