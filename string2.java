ebaf961ae053d8deff6515dd8d4dbb6b01e5180a
rakshithamg-max <rakshithamg70@gmail.com>
2026-04-21 07:21:26 +1400

    Update string2.java

--- string2.java
+++ string2.java
@@ -1,4 +1,6 @@
- package AJ2;
+/*2b. Develop a Java program to make a Performance Test on StringBuffer and StringBuilder for appending the string ‘AIET’ for 10000 times 
+both in the StringBuffer and StringBuilder. Justify your answer which one is better.*/
+package AJ2;
 public class string2 {
     public static void main(String[] args) {
 
