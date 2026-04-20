aecbdd8a323b5ceb4c675eebbbd098530e1a1bf9
rakshithamg-max <rakshithamg70@gmail.com>
2026-04-21 07:38:55 +1400

    Update WordCounter.java

--- WordCounter.java
+++ WordCounter.java
@@ -1,3 +1,6 @@
+/* Write a Java Program for Counting the number of words in a string using user defined function countWords()
+*/
+
 package prog3c;
 public class WordCounter {
     public static int countWords(String str) {
